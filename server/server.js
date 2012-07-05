1109690d0efb85cea74623d3aecdaf787abc3984
Ryan Harrell <rharrell@southern.edu>
2012-07-05 14:24:41 -0400

    Removed callback

--- server/server.js
+++ server/server.js
@@ -61,13 +61,7 @@ Meteor.methods({
             console.log(mailOptions);
             console.log(chatTranscript);
             
-            transport.sendMail(mailOptions, function(error,result){
-                if (error) {
-                    return 'Error sending email!  Error was: ' + error;
-                } else {
-                    return 'Email response successfull!  Response was: ' + result +'\nEmail to: ' + email + '\nEmail text: \n' + chatTranscript;
-                }
-            });
+            transport.sendMail(mailOptions);
     }
 });
 
