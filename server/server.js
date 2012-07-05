738875f63f5b9fe2d85a21a7c9a0daa2e9332d34
Ryan Harrell <rharrell@southern.edu>
2012-07-05 15:43:07 -0400

    bugfix

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ Meteor.methods({
 var generateHTMLChat = function(roomId) {
     var html = '<div id="emailBody"><ul style="list-style:none;margin:0px;padding:0px;width:650px;">';
     var messages = Messages.find({roomId:roomId});
-    messages.forEach(message) {
+    messages.forEach(function(message) {
         if (message.content) {
             html += '<li class="' + message.role + '" style="border-bottom:1px solid #CCC;padding:4px;">';
             if (message.role == 'host') {
@@ -84,7 +84,7 @@ var generateHTMLChat = function(roomId) {
             }
             html += '<strong>' + message.user + '</strong> <em>(' + message.timestamp + ')</em></span>: ' + message.content + '</li>';
         }
-    }
+    });
 
     html += '</ul></div>';
 
