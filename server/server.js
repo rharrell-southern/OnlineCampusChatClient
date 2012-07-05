e59e6b4f197179b91c87e3aa57ec7553ea167572
Ryan Harrell <rharrell@southern.edu>
2012-07-05 14:14:54 -0400

    Re-enable mail function

--- server/server.js
+++ server/server.js
@@ -60,15 +60,14 @@ Meteor.methods({
             }
             console.log(mailOptions);
             console.log(chatTranscript);
-            return 'Email response successfull!\nEmail to: ' + email + '\nEmail text: \n' + chatTranscript;
-            /*
+            
             transport.sendMail(mailOptions, function(error,result){
                 if (error) {
                     return 'Error sending email!  Error was: ' + error;
                 } else {
                     return 'Email response successfull!  Response was: ' + result +'\nEmail to: ' + email + '\nEmail text: \n' + chatTranscript;
                 }
-            });*/
+            });
     }
 });
 
