2234adf81f9015107a4536ad7fa98177495699f8
Ryan Harrell <rharrell@southern.edu>
2012-07-06 10:41:08 -0400

    Loging console

--- client/chat.js
+++ client/chat.js
@@ -194,6 +194,7 @@ Meteor.startup(function(){
             }
           },
           changed: function (item,index,oldItem) {
+            console.log(oldItem);
             if(item.active == true  && oldItem.active == 'false') {
                 console.log("ring! ring!");
                 ring.play();
