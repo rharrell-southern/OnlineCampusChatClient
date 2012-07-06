c7579eabd102e6c4c17bc4006ba237ed1d8a35bf
Ryan Harrell <rharrell@southern.edu>
2012-07-06 10:43:02 -0400

    debug code

--- client/chat.js
+++ client/chat.js
@@ -194,7 +194,7 @@ Meteor.startup(function(){
             }
           },
           changed: function (item,index,oldItem) {
-            console.log(oldItem);
+            console.log("Old: " + oldItem.active + " New: " + item.active);
             if(item.active == true  && oldItem.active == 'false') {
                 console.log("ring! ring!");
                 ring.play();
