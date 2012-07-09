ea93f1888b0b043aefc9d7551bfcb50deb585fe9
Ryan Harrell <rharrell@southern.edu>
2012-07-09 10:16:13 -0400

    Added host to it's own template

--- client/room.js
+++ client/room.js
@@ -8,7 +8,7 @@ Template.RoomList.rooms = function (){
     }
     return Rooms.find(filter,{sort:{active:-1,unread:-1,moodleClass:1,userId:1}});
 };
-Template.RoomList.hosts = function (){
+Template.HostList.hosts = function (){
     return Hosts.find({},{sort:{host:1}});
 };
 Template.RoomList.events = {
