ca2d64183122a9f8fefbaf3f8d212a1088ea8a22
Ryan Harrell <rharrell@southern.edu>
2012-07-09 09:22:36 -0400

    bugfix

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ Meteor.methods({
         if (!roomsResult) {
             Fiber(function(){
                 Rooms.insert({userId:name,moodleClass:moodleClass,active:false,unread:0,host:null,typing:whoTyping},function(error,result){  
-                    Rooms.update({result},{$set:{active:true}});
+                    Rooms.update({_id:result},{$set:{active:true}});
                     var d = new Date();
                     var date = d.toDateString() + " " + d.toLocaleTimeString();  
                     Messages.insert({roomId: result ,content: 'Thank you for accessing Southern Adventist University Online Campus Support, someone will be with you shortly.  You may begin by entering any questions here and our support staff will see them when they join you.', user: 'Online Support',role:'welcome',messagetime:0,date:date});
